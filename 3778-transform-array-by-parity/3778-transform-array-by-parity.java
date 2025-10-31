2caaf9c61e397319b352089e5312cb3a777f9bfa
keshav kourav <160245801+keshavkourav@users.noreply.github.com>
2025-10-31 20:40:00 +0530

    Time: 1 ms (100%), Space: 45.6 MB (7.29%) - LeetHub

---
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] transformArray(int[] nums) {
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]%2==0){
+                nums[i]=0;
+            }
+            else{
+                nums[i]=1;
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            for (int j=i+1; j<nums.length; j++){
+                if (nums[i]>nums[j]){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
