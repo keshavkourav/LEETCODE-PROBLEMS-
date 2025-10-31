a327aff44cfca88ab120ab342aaf35155ce947f9
keshav kourav <160245801+keshavkourav@users.noreply.github.com>
2025-10-31 20:40:39 +0530

    Time: 353 ms (5%), Space: 46.5 MB (83.23%) - LeetHub

---
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for ( int i=0; i<nums.length; i++){
+                nums[i]=(nums[i] * nums[i]);
+        }
+
+        for (int i=0; i<nums.length-1; i++){
+            for(int j=i; j<nums.length; j++)
+            if(nums[i]>nums[j]){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+
+        }
+        return nums;
+        
+    }
+}
